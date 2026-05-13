c8ebc21e761b3160e13d8c458748fde119f9a6f3
reskuyyy <reskygilang07@gmail.com>
2026-05-13 19:27:45 +0700

    Update so.js

--- api/so.js
+++ api/so.js
@@ -1,15 +1,36 @@
 export default async function handler(req, res) {
+
+  const {
+    storeId = 'N497',
+    dateSo = '12-05-2026',
+    typeSo = 'GRAND'
+  } = req.query;
+
+  const url =
+    `https://app.alfastore.co.id/prd/api/rpt/laporan_so/prosentase_so?storeId=${storeId}&dateSo=${dateSo}&typeSo=${typeSo}`;
+
   try {
-    const response = await fetch(
-      'https://app.alfastore.co.id/prd/api/rpt/laporan_so/prosentase_so?storeId=N497&dateSo=12-05-2026&typeSo=GRAND'
-    );
 
-    const data = await response.json();
-
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
+    const response = await fetch(url, {
+      headers: {
+        'accept': '*/*'
+      }
     });
+
+    const text = await response.text();
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+
+    res.status(200).send(text);
+
+  } catch (err) {
+
+    res.status(500).send({
+      message: err.message,
+      stack: err.stack
+    });
+
   }
+
 }
