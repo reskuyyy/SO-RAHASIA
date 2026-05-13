ff85932217b3338fcfdcd4906352c3e767cab29e
reskuyyy <reskygilang07@gmail.com>
2026-05-13 19:35:22 +0700

    Update so.js

--- api/so.js
+++ api/so.js
@@ -2,25 +2,26 @@ export default async function handler(req, res) {
 
   try {
 
-    const storeId = req.query.storeId || 'N497';
-    const dateSo = req.query.dateSo || '12-05-2026';
-    const typeSo = req.query.typeSo || 'GRAND';
+    const storeId =
+      req.query.storeId || 'N497';
 
-    const params = new URLSearchParams({
-      storeId,
-      dateSo,
-      typeSo
-    });
+    const dateSo =
+      req.query.dateSo || '12-05-2026';
+
+    const typeSo =
+      req.query.typeSo || 'GRAND';
 
     const url =
-      'https://app.alfastore.co.id/prd/api/rpt/laporan_so/prosentase_so?' +
-      params.toString();
+      `https://app.alfastore.co.id/prd/api/rpt/laporan_so/prosentase_so?storeId=${storeId}&dateSo=${dateSo}&typeSo=${typeSo}`;
 
     const response = await fetch(url);
 
     const text = await response.text();
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      '*'
+    );
 
     res.status(200).send(text);
 
